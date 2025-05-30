a449117371603c88ae817d3690318c6b745d9241
Navinder Gill <126389118+navigill7@users.noreply.github.com>
2025-05-30 19:54:08 +0530

    adding alb file

---
+++ ALB/alb.tf
@@ -0,0 +1,39 @@
+resource "aws_lb" "test" {
+  name               = "medusa-test-lb-tf"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = var.security_group_id
+  subnets            = var.subnet_ids
+  tags = {
+    Environment = "production"
+  }
+}
+
+resource "aws_lb_target_group" "TG" {
+  name        = "tf-example-lb-tg"
+  port        = 6500
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+  target_type = "ip"
+  deregistration_delay = "300"
+
+  health_check {
+    path                = "/"  # Verify path
+    protocol            = "HTTP"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "Listener" {
+  load_balancer_arn = aws_lb.test.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.TG.arn
+  }
+}
