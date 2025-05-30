29d9d47ed92172e47e776c32c089d8d3779c2b80
Navinder Gill <126389118+navigill7@users.noreply.github.com>
2025-05-30 19:52:28 +0530

    creating service file for medusa deployment on ecs

---
+++ ECS/service_file.tf
@@ -0,0 +1,17 @@
+resource "aws_ecs_service" "medusa-cluster-service" {
+  name            = "cluster-service"
+  cluster         = aws_ecs_cluster.medusa-cluster.id
+  task_definition = aws_ecs_task_definition.TD.arn
+  desired_count   = 2
+  launch_type     = "FARGATE"
+  network_configuration {
+    subnets          = var.subnet_ids
+    security_groups  = var.security_group_id
+    assign_public_ip = true
+  }
+  load_balancer {
+    target_group_arn = var.target_group_arn
+    container_name   = "medusa"
+    container_port   = 3000
+  }
+}
