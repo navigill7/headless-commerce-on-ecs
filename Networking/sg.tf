c4c826aae85f23e950d4f553263452d6438b094b
Navinder Gill <126389118+navigill7@users.noreply.github.com>
2025-06-01 18:43:57 +0530

    changing the sg port

--- Networking/sg.tf
+++ Networking/sg.tf
@@ -18,9 +18,9 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
 resource "aws_vpc_security_group_ingress_rule" "allow_ecs_from_alb" {
   security_group_id        = aws_security_group.allow_tls.id
   referenced_security_group_id = aws_security_group.allow_tls.id
-  from_port                = 6500
+  from_port                = 9000
   ip_protocol              = "tcp"
-  to_port                  = 6500
+  to_port                  = 9000
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
